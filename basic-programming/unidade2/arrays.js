ef5d94aa0e13c144102294e9b694b749582afb91
msilva <marcia.silva1802@gmail.com>
2018-05-24 15:31:04 -0300

    calculadora com a media de tres campos

--- basic-programming/unidade2/arrays.js
+++ basic-programming/unidade2/arrays.js
@@ -1,1 +1,11 @@
 
+var idade = [80, 60, 45, 20, 7, 1];
+for (var co = 0; co < idade.length; co = co + 1) {
+  console.log(idade[co]);
+}
+
+//vantagem de usar o for ao invés de while:
+var idades = [80, 60, 45, 20, 7, 1];
+for (printIdade of idades) {
+  console.log(printIdade);
+}
