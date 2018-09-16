cbc9134ec28ff012285fc4fee5ce4ff12b595f0a
msilva <marcia.silva1802@gmail.com>
2018-09-16 18:41:10 -0300

    refatoracao ES6 puro, acentos, números e quebra de linha adicionados

--- 01.basic-programming/cifra-de-cesar-buttons/app.js
+++ 01.basic-programming/cifra-de-cesar-buttons/app.js
@@ -1,74 +1,100 @@
-var message;
-var charcodedMensage;
+window.onload = () => {
+  document.getElementById('cypher').disabled = true;
+  document.getElementById('decypher').disabled = true;
 
-function cypher() {
-  message = askMessage();
-  charcodedMessage = transformInCharChode(message);
-  var cipheredCharCode = cipherCharCode(charcodedMessage);
-  var encryptedMessage = transformInLetters(cipheredCharCode);
-  return document.getElementById('message-result').innerHTML = '<p><font color=#303030> Esta é a sua mensagem criptografada: </font><p> "' + encryptedMessage + '"';
+  document.getElementById('message-box').addEventListener('input', () => {
+    enableButtons(); 
+    addTextAreaRows();
+    clearResults();
+  });
+  document.getElementById('cypher').addEventListener('click', cypher);
+  document.getElementById('decypher').addEventListener('click', decypher);
 }
 
-function decypher() {
-  message = askMessage();
-  charcodedMessage = transformInCharChode(message);
-  var decipheredCharCode = decipherCharCode(charcodedMessage);
-  var decryptedMessage = transformInLetters(decipheredCharCode);
-  return document.getElementById('message-result').innerHTML = '<p><font color=#303030> Esta é a sua mensagem descriptografada: </font><p> "' + decryptedMessage + '"';
-}
-
-function askMessage() {
-  do {
-    message = prompt('Escreva uma mensagem: \n *O texto não pode conter números, pontuação, "ç" e acentos.');
-
-    if (message.length === 0) {
-      alert('Este campo não pode ficar vazio.');
-    } else if (isNaN(parseInt(message)) === false) {
-      alert('Este campo não aceita números.');
-    } else if (message.length > 0 && isNaN(parseInt(message)) === true) {
-      message.split('');
-      return message;
-    }
-  } while (message.length === 0 || isNaN(parseInt(message)) === false);
-}
-
-function transformInCharChode(message) {
-  var aux1 = [];
-  for (eachLetter of message) {
-    aux1.push(eachLetter.charCodeAt());
+const enableButtons = () => {
+  if (getMessage().trim().length !== 0) {
+    document.getElementById('cypher').disabled = false;
+    document.getElementById('decypher').disabled = false;
+  } else {
+    document.getElementById('cypher').disabled = true;
+    document.getElementById('decypher').disabled = true;
   }
-  return aux1;
 }
 
-function cipherCharCode(charcodedMessage) {
-  var aux2 = [];
-  for (charCode of charcodedMessage) {
-    if (charCode > 96) {
-      aux2.push((charCode - 97 + 33) % 26 + 97);
-    } else if (charCode < 91 && charCode !== 32) {
-      aux2.push((charCode - 65 + 33) % 26 + 65);
-    } else {
-      aux2.push(32);
-    }
+const addTextAreaRows = () => {
+  const txtArea = document.getElementById('message-box');
+  txtArea.style.height = '8vh';
+  txtArea.style.height = txtArea.scrollHeight + 'px';
+}
+
+const clearResults = () => {
+  if (document.getElementById('p-group')) 
+    document.getElementById('results').removeChild(document.getElementById('p-group'));
+}
+
+const cypher = () => {
+  const cypheredCharCodes = cypherCharcodes(getCharcodesFromMessage());
+  const resultMessage = transformIntoLetters(cypheredCharCodes);
+  const method = 'criptografada';
+  showResultOnPage(resultMessage, method);
+  clearTextArea();
+}
+
+const decypher = () => {
+  const decypheredCharCodes = decypherCharcodes(getCharcodesFromMessage());
+  const resultMessage = transformIntoLetters(decypheredCharCodes);
+  const method = 'descriptografada';
+  showResultOnPage(resultMessage, method);
+  clearTextArea();
+}
+
+const getMessage = () => document.getElementById('message-box').value;
+const getCharcodesFromMessage = () => getMessage().split('').map(val => val.charCodeAt());
+
+const cypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
+
+  if (charCode > 47 && charCode < 58) {
+    charCode = (charCode - 48 + 33) % 10 + 48;
+  } else if (charCode > 64 && charCode < 91) {
+    charCode = (charCode - 65 + 33) % 26 + 65;
+  } if (charCode > 96 && charCode < 192) {
+    charCode = (charCode - 97 + 33) % 26 + 97;
+  }  else if (charCode > 191 && charCode < 222) {
+    charCode = (charCode - 192 + 33) % 30 + 192;
+  } else if (charCode > 221 && charCode <=255) {
+    charCode = (charCode - 222 + 33) % 34 + 222;
+  } 
+  return charCode;
+});
+
+const decypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
+
+  if (charCode > 47 && charCode < 58) {
+    charCode = (charCode - 57 - 33) % 10 + 57;
+  } else if (charCode > 64 && charCode < 91) {
+    charCode = (charCode - 90 - 33) % 26 + 90;
+  } else if (charCode > 96 && charCode < 192) {
+    charCode = (charCode - 122 - 33) % 26 + 122;
+  } else if (charCode > 191 && charCode < 222) {
+    charCode = (charCode - 221 - 33) % 30 + 221;
+  } else if (charCode > 221 && charCode <=255) {
+    charCode = (charCode - 255 - 33) % 34 + 255;
   }
-  return aux2;
+  return charCode;
+});
+
+const transformIntoLetters = charcodesArray => String.fromCharCode.apply(this, charcodesArray);
+
+const showResultOnPage = (resultMessage, method) => {
+  return document.getElementById('results').innerHTML = `
+  <div id="p-group">
+    <p class="result-fixed-message w-100">Esta é a sua mensagem ${method}:</p>
+    <p class="result-text w-100">${resultMessage}</p>
+  </div>`;
 }
 
-function transformInLetters(array) {
-  var aux3 = String.fromCharCode.apply(this, array);
-  return aux3;
-}
-
-function decipherCharCode(charcodedMessage) {
-  var aux4 = [];
-  for (charCode of charcodedMessage) {
-    if (charCode > 96) {
-      aux4.push((charCode - 122 - 33) % 26 + 122);
-    } else if (charCode < 91 && charCode !== 32) {
-      aux4.push((charCode - 90 - 33) % 26 + 90);
-    } else {
-      aux4.push(32);
-    }
-  }
-  return aux4;
+const clearTextArea = () => { 
+  document.getElementById('message-box').value = '';
+  enableButtons();
+  addTextAreaRows();
 }
