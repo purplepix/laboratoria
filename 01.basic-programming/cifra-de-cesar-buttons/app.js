a0752b450490ebb1f340d62e8e37352443375a92
msilva <marcia.silva1802@gmail.com>
2018-09-16 22:24:59 -0300

    Atualiza o arquivo README e corrije pequenos erros no código

--- 01.basic-programming/cifra-de-cesar-buttons/app.js
+++ 01.basic-programming/cifra-de-cesar-buttons/app.js
@@ -9,7 +9,7 @@ window.onload = () => {
   });
   document.getElementById('cypher').addEventListener('click', cypher);
   document.getElementById('decypher').addEventListener('click', decypher);
-}
+};
 
 const enableButtons = () => {
   if (getMessage().trim().length !== 0) {
@@ -19,18 +19,18 @@ const enableButtons = () => {
     document.getElementById('cypher').disabled = true;
     document.getElementById('decypher').disabled = true;
   }
-}
+};
 
 const addTextAreaRows = () => {
   const txtArea = document.getElementById('message-box');
   txtArea.style.height = '8vh';
   txtArea.style.height = txtArea.scrollHeight + 'px';
-}
+};
 
 const clearResults = () => {
   if (document.getElementById('p-group')) 
     document.getElementById('results').removeChild(document.getElementById('p-group'));
-}
+};
 
 const cypher = () => {
   const cypheredCharCodes = cypherCharcodes(getCharcodesFromMessage());
@@ -38,7 +38,7 @@ const cypher = () => {
   const method = 'criptografada';
   showResultOnPage(resultMessage, method);
   clearTextArea();
-}
+};
 
 const decypher = () => {
   const decypheredCharCodes = decypherCharcodes(getCharcodesFromMessage());
@@ -46,29 +46,27 @@ const decypher = () => {
   const method = 'descriptografada';
   showResultOnPage(resultMessage, method);
   clearTextArea();
-}
+};
 
 const getMessage = () => document.getElementById('message-box').value;
 const getCharcodesFromMessage = () => getMessage().split('').map(val => val.charCodeAt());
 
 const cypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
-
   if (charCode > 47 && charCode < 58) {
     charCode = (charCode - 48 + 33) % 10 + 48;
   } else if (charCode > 64 && charCode < 91) {
     charCode = (charCode - 65 + 33) % 26 + 65;
   } if (charCode > 96 && charCode < 192) {
     charCode = (charCode - 97 + 33) % 26 + 97;
-  }  else if (charCode > 191 && charCode < 222) {
+  } else if (charCode > 191 && charCode < 222) {
     charCode = (charCode - 192 + 33) % 30 + 192;
-  } else if (charCode > 221 && charCode <=255) {
+  } else if (charCode > 221 && charCode <= 255) {
     charCode = (charCode - 222 + 33) % 34 + 222;
   } 
   return charCode;
 });
 
 const decypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => {
-
   if (charCode > 47 && charCode < 58) {
     charCode = (charCode - 57 - 33) % 10 + 57;
   } else if (charCode > 64 && charCode < 91) {
@@ -77,7 +75,7 @@ const decypherCharcodes = charcodesArray => charcodesArray.map((charCode, i) => 
     charCode = (charCode - 122 - 33) % 26 + 122;
   } else if (charCode > 191 && charCode < 222) {
     charCode = (charCode - 221 - 33) % 30 + 221;
-  } else if (charCode > 221 && charCode <=255) {
+  } else if (charCode > 221 && charCode <= 255) {
     charCode = (charCode - 255 - 33) % 34 + 255;
   }
   return charCode;
@@ -91,10 +89,10 @@ const showResultOnPage = (resultMessage, method) => {
     <p class="result-fixed-message w-100">Esta é a sua mensagem ${method}:</p>
     <p class="result-text w-100">${resultMessage}</p>
   </div>`;
-}
+};
 
 const clearTextArea = () => { 
   document.getElementById('message-box').value = '';
   enableButtons();
   addTextAreaRows();
-}
+};
